0ba3c3401821e4e1c31aa943b1c51ba18a4ffd66
Mitch Robinson <mitch@scribeless.co>
2022-10-22 22:47:01 +0100

    ui: wrap blob photos in relative div

--- src/sections/HeaderSection.vue
+++ src/sections/HeaderSection.vue
@@ -7,15 +7,17 @@ import LottieWoods from '@/assets/LottieWoods.jpeg'
 <template>
   <header
     class="container relative flex h-screen snap-center snap-always flex-col items-center justify-end space-y-1 overflow-hidden px-6 py-8">
-    <div class="absolute top-0 aspect-video h-28 w-[90%] max-w-xs">
-      <ColouredBlob
-        class="absolute w-[200%] -translate-y-[15%] -translate-x-1/4 -rotate-45" />
-      <img
-        :src="LottieWoods"
-        class="absolute -right-10 top-[48%] w-[66%] max-w-[220px] -rotate-6 shadow-2xl" />
-      <img
-        :src="RedSmile"
-        class="absolute -left-12 top-[25%] w-[70%] max-w-[220px] rotate-12 shadow-2xl" />
+    <div class="absolute top-12 aspect-video h-28 w-[90%] max-w-xs">
+      <div class="relative h-full w-full">
+        <ColouredBlob
+          class="absolute w-[200%] max-w-xl -translate-y-[15%] -translate-x-1/4 -rotate-45" />
+        <img
+          :src="LottieWoods"
+          class="absolute -right-10 top-[48%] w-[66%] max-w-[220px] -rotate-6 shadow-2xl" />
+        <img
+          :src="RedSmile"
+          class="absolute -left-12 top-[25%] w-[70%] max-w-[220px] rotate-12 shadow-2xl" />
+      </div>
     </div>
 
     <div class="z-10 flex w-full flex-col space-y-1">
