293bd0a36b496a010cad1fafa2c6f3a9d89b086d
Mitch Robinson <mitch@scribeless.co>
2022-10-22 22:35:51 +0100

    ui: Set snap to center

--- src/sections/ActingSection.vue
+++ src/sections/ActingSection.vue
@@ -4,7 +4,7 @@ import KneesUps from '@/assets/KneesUp.jpeg'
 
 <template>
   <section
-    class="container relative flex h-screen snap-start snap-always flex-col items-start justify-start space-y-8 overflow-visible px-6 py-8">
+    class="container relative flex h-screen snap-center snap-always flex-col items-start justify-start space-y-8 overflow-visible px-6 py-8">
     <h2 class="text-4xl font-light lowercase text-gray-700">Acting</h2>
     <p class="text-xl font-light lowercase text-gray-700">
       Ut sagittis ipsum ac nulla sagittis, quis venenatis arcu varius. Morbi
