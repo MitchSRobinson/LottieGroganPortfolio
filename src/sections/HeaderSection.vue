b8a03831caa953bed5b5fd8e999cf885fa002989
Mitch Robinson <mitch@scribeless.co>
2022-10-22 22:28:21 +0100

    ui: Fix header blob container size

--- src/sections/HeaderSection.vue
+++ src/sections/HeaderSection.vue
@@ -7,7 +7,7 @@ import LottieWoods from '@/assets/LottieWoods.jpeg'
 <template>
   <header
     class="container relative flex h-screen snap-start snap-always flex-col items-center justify-between space-y-1 overflow-visible px-6 py-8">
-    <div class="relative top-0 aspect-video h-1/3 w-[90%] max-w-xs">
+    <div class="relative top-0 aspect-video h-28 w-[90%] max-w-xs">
       <ColouredBlob
         class="absolute w-[200%] -translate-y-[15%] -translate-x-1/4 -rotate-45" />
       <img
